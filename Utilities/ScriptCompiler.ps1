d86d477e3ae23b0c2e06532fdf06797257c1aa6b
AlienXAXS <mark.phillips@axsemail.co.uk>
2017-12-18 22:08:35 +0000

    Shall we play on xmountain?

--- Utilities/ScriptCompiler.ps1
+++ Utilities/ScriptCompiler.ps1
@@ -87,7 +87,7 @@ if ( (Test-Path $CompiledMutatorPath -PathType Any) -eq $true )
     Write-Output "Build Passed, copying to Renx"
     Copy-Item ($CompiledMutatorPath) $FinalMutatorPath
     Write-Output "Starting Game"
-    Start-Process $RenXBinary -ArgumentList "CNC-Lakeside?Mutator=$MutatorStartupParam $RenXAdditionalStartupParams"
+    Start-Process $RenXBinary -ArgumentList "CNC-xMountain?Mutator=$MutatorStartupParam $RenXAdditionalStartupParams"
 } else {
     Write-Output " --------- BUILD FAILED ----------- "
 }
